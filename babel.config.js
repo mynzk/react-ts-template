bdeff10aab8fb21f754f405b07a400480a6d6302
zhang kui <1627528424@qq.com>
2020-05-25 18:44:19 +0800

    add first :bee:

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                useBuiltIns: 'usage',
+                corejs: 3, // 2-corejs@2  3-corejs@3
+            },
+        ],
+        '@babel/preset-typescript',
+        '@babel/preset-react',
+    ],
+    plugins: [
+        ['@babel/plugin-transform-runtime', { corejs: 3 }],
+        [
+            'import',
+            {
+                libraryName: 'antd',
+                libraryDirectory: 'lib',
+                style: true,
+            },
+        ],
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+        '@babel/plugin-syntax-dynamic-import',
+    ],
+};
